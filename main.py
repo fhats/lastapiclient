8f1a43551ee9dbf3cc4fdefb3e74a641acd6b194
Fred <fxh32@case.edu>
2011-01-19 17:29:15 -0500

    Project is in a presentable state ...?

--- main.py
+++ main.py
@@ -14,11 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import cgi
+import logging
+import simplejson as json
 import os
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
+from lastfmapi import getLastFmJson
 
 
 class MainHandler(webapp.RequestHandler):
@@ -27,15 +31,42 @@ class MainHandler(webapp.RequestHandler):
 
 class TrackHandler(webapp.RequestHandler):
     def get(self):
+        self.response.out.write( render_template('track_info.html', {}) )
         
     def post(self):
-        cgi.escape(self.request.get('track')
+        track_name = cgi.escape(self.request.get('track'))
+        artist_name = cgi.escape(self.request.get('artist'))
+        
+        lastfm_args = {
+            'method': 'track.getInfo',
+            'artist': artist_name,
+            'track': track_name
+        }
+        lastfm_response = getLastFmJson(lastfm_args)
+        
+        self.response.out.write( render_template('track_info.html', lastfm_response) )
         
 class ArtistHandler(webapp.RequestHandler):
     def get(self):
-    
+        self.response.out.write( render_template('artist_info.html', {}) )
+        
     def post(self):
-    
+        artist_name = cgi.escape(self.request.get('artist'))
+        
+        lastfm_args = {
+            'method': 'artist.getInfo',
+            'artist': artist_name
+        }
+        lastfm_response = getLastFmJson(lastfm_args)
+        
+        logging.info(json.dumps(lastfm_response, sort_keys=True, indent=4))
+        
+        self.response.out.write( render_template('artist_info.html', lastfm_response) )
+
+def render_template(file, args):
+    path = os.path.join(os.path.dirname(__file__), 'templates', file)
+    return template.render(path, args)
+        
 def main():
     application = webapp.WSGIApplication([('/', MainHandler),
                                           ('/track/info', TrackHandler),
